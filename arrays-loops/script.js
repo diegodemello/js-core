9e46a6ad28b1e3824b23047051b046c30c4e13ac
Diego de Mello <diegodemello027@gmail.com>
2025-06-13 22:19:52 -0300

    foreach

--- arrays-loops/script.js
+++ arrays-loops/script.js
@@ -17,3 +17,51 @@ for(let i = 0; i < frutas.length; i++){
 
 let melancia = frutas[4];
 console.log(melancia);
+
+// -----------
+
+const listaDeCompras = ['Pão', 'Leite', 'Ovos', 'Café'];
+
+listaDeCompras.forEach((item) => {
+    console.log(`Item: ${item}`);
+});
+
+
+// ----------
+
+const numeros = [20, 30, 40, 50, 60];
+let soma = 0;
+
+numeros.forEach((item, index) => {
+    console.log(`[${index}]: ${item}`);
+    soma = soma + item;
+});
+console.log(soma);
+
+// ---------
+
+let vestuario = 0;
+let eletronicos = 0;
+let eletrodomesticos = 0;
+
+const produtos = [
+    {nome: 'Camisa', categoria: 'Vestuário'},
+    {nome: 'Notebook', categoria: 'Eletrônicos'},
+    {nome: 'Smartphone', categoria: 'Eletrônicos'},
+    {nome: 'Calça', categoria: 'Vestuário'},
+    {nome: 'Geladeira', categoria: 'Eletrodomésticos'},
+];
+
+produtos.forEach((item) => {
+    if(item.categoria === 'Vestuário'){
+        vestuario += 1;
+    }else if(item.categoria === 'Eletrônicos'){
+        eletronicos += 1;
+    }else{
+        eletrodomesticos += 1;
+    }
+});
+
+console.log(`Vestuário: ${vestuario}`);
+console.log(`Eletrônicos: ${eletronicos}`);
+console.log(`Eletrodomésticos: ${eletrodomesticos}`);
